c6a9ffb512ec89b556c2ca31db1065575c11707c
wanghao40 <wanghao40@kuaishou.com>
2024-12-25 11:42:20 +0800

    Initial Commit

---
+++ love-song-ios-2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  love-song-ios-2
+//
+//  Created by 王昊 on 2024/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ love-song-ios-2/love_song_ios_2App.swift
@@ -0,0 +1,17 @@
+//
+//  love_song_ios_2App.swift
+//  love-song-ios-2
+//
+//  Created by 王昊 on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct love_song_ios_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
